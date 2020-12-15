90721ba702a2bfa4fd8649914758afb3aac2fbf1
Nathan Santos <nathan@w16.com.br>
2020-12-14 22:16:57 -0300

    Remove logo.svg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        PodJS
-      </header>
+      <header className="App-header">PodJS</header>
     </div>
   );
 }
