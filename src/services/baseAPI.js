73cddece34065d042c28c6dc4082d5746a7c3580
Nathan Silva Santos <nathan@w16.com.br>
2022-01-22 21:56:24 -0300

    unlock gh-pages

--- src/services/baseAPI.js
+++ src/services/baseAPI.js
@@ -2,7 +2,7 @@
 import Axios from "axios";
 
 const baseAPI = Axios.create({
-  baseURL: `https://cors.io/?${process.env.REACT_APP_PODCAST_API_URL}`,
+  baseURL: process.env.REACT_APP_PODCAST_API_URL,
   timeout: 30000,
   validateStatus: (status) => status !== 401,
 });
