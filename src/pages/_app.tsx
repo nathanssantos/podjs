28eee8d1e8ca986f1eddd809597ec1d12646da37
Nathan Silva Santos <nathan@w16.com.br>
2022-06-11 21:56:54 -0300

    chakra ui setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../theme/theme';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { Button } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +11,9 @@ const Home: NextPage = () => {
         <meta name='author' content='Nathan Silva Santos <nathansilvasantos@gmail.com>' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>PodJS</main>
+      <main>
+        <Button colorScheme='blue'>PodJS</Button>
+      </main>
     </div>
   );
 };
