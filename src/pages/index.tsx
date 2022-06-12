502deb55b3c1eee5abe42650674f999ce1850cf1
Nathan Silva Santos <nathan@w16.com.br>
2022-06-11 22:31:28 -0300

    mobx store setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Button } from '@chakra-ui/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
+import { useStore } from '../hooks';
 
 const Home: NextPage = () => {
+  const store = useStore();
+
   return (
     <div>
       <Head>
@@ -12,7 +15,15 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <Button colorScheme='blue'>PodJS</Button>
+        <Flex direction='column' gap='2'>
+          <Flex direction='column'>
+            <Text>{store.authStore.user?.name}</Text>
+            <Text>{store.authStore.user?.email}</Text>
+          </Flex>
+          <Button colorScheme='blue' alignSelf='flex-start'>
+            PodJS
+          </Button>
+        </Flex>
       </main>
     </div>
   );
