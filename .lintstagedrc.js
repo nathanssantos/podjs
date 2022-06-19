6b9a92fda59f74adba624ae5763484ade5a1b8d8
Nathan Silva Santos <nathan@w16.com.br>
2022-06-19 18:24:10 -0300

    liststaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,16 @@
 const path = require('path');
 
-const buildEslintCommand = (filenames) =>
+const eslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+const prettierCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': [eslintCommand],
+  '{,!(node_modules|public|build|dist|.next)/**/}*.{js,jsx,ts,tsx,scss,css}': [prettierCommand],
 };
