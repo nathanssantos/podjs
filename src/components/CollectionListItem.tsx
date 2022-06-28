a03c37f5ebd697bced6346ec81a587f4e5e33ba4
Nathan Silva Santos <nathan@w16.com.br>
2022-06-28 18:18:14 -0300

    grid spacing

--- src/components/CollectionListItem.tsx
+++ src/components/CollectionListItem.tsx
@@ -64,7 +64,7 @@ const CollectionListItem = (props: CollectionListItemProps) => {
       borderRadius='lg'
       overflow='hidden'
       role='group'
-      maxW='308px'
+      maxW={{ sm: '308px' }}
     >
       <Flex cursor='pointer' onClick={handleClick}>
         <Image
