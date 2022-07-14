2a9c39235b64c879ad19cebb8371ee951684898a
Nathan Silva Santos <nathan@w16.com.br>
2022-07-14 00:12:44 -0300

    podcast list items improvements

--- src/components/PodcastListItem.tsx
+++ src/components/PodcastListItem.tsx
@@ -14,6 +14,7 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 import { observer } from 'mobx-react';
+import { MouseEvent } from 'react';
 import {
   RiDeleteBinLine,
   RiInformationLine,
@@ -60,6 +61,7 @@ const PodcastListItem = (props: PodcastListItemProps) => {
   const removeFromPlayList = () => {
     removePodcastFromPlaylist(podcast);
   };
+
   const menu = (
     <Menu>
       <MenuButton
@@ -99,11 +101,19 @@ const PodcastListItem = (props: PodcastListItemProps) => {
   );
 
   return (
-    <Flex borderBottomWidth='1px' pb={6} position='relative' gap={4}>
+    <Flex
+      borderBottomWidth='1px'
+      pb={6}
+      position='relative'
+      gap={4}
+      onClick={playPodcast}
+      cursor='pointer'
+    >
       <Flex
         direction={{ base: 'column', md: 'row' }}
         flex={1}
         alignItems={{ base: 'center', md: 'initial' }}
+        gap={4}
       >
         <Flex
           borderWidth='1px'
@@ -114,8 +124,6 @@ const PodcastListItem = (props: PodcastListItemProps) => {
           minW='180px'
           h={{ h: 'initial', md: '180px' }}
           maxW='320px'
-          onClick={playPodcast}
-          cursor='pointer'
           alignSelf={{ md: 'flex-start' }}
         >
           <Image
@@ -129,131 +137,110 @@ const PodcastListItem = (props: PodcastListItemProps) => {
         </Flex>
 
         <Flex direction='column' flex={1}>
-          <Flex justify='space-between'>
+          <Flex justify='space-between' gap={4}>
             <Text
               fontWeight='semibold'
               lineHeight='tight'
               color={currentPodcast?.enclosure?.url === enclosure?.url ? 'teal.300' : ''}
-              onClick={playPodcast}
-              cursor='pointer'
               flex={1}
-              pl={{ base: 0, md: 4 }}
-              pt={{ base: 4, md: 1 }}
-              pb={2}
-              pr={4}
             >
               {title}
             </Text>
-            <Flex direction='column'>
-              <Flex pt={{ base: 4, md: 0 }}>{menu}</Flex>
-              <Flex flex={1} onClick={playPodcast} cursor='pointer' />
+            <Flex onClick={(e: MouseEvent<HTMLDivElement>) => e.stopPropagation()}>{menu}</Flex>
+          </Flex>
+          <Flex direction='column' gap={2}>
+            <Flex align='center' gap={2} w='100%'>
+              {currentPodcast?.enclosure?.url === enclosure?.url && (
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='20px'
+                  height='20px'
+                  viewBox='0 0 100 100'
+                  preserveAspectRatio='xMidYMid'
+                >
+                  <g transform='rotate(180 50 50)'>
+                    <rect x='15' y='15' width='10' height='40' fill='#4fd1c5'>
+                      <animate
+                        attributeName='height'
+                        values='50;70;30;50'
+                        keyTimes='0;0.33;0.66;1'
+                        dur='1s'
+                        repeatCount='indefinite'
+                        calcMode='spline'
+                        keySplines='0.5 0 0.5 1;0.5 0 0.5 1;0.5 0 0.5 1'
+                        begin='-0.4s'
+                      ></animate>
+                    </rect>
+                    <rect x='35' y='15' width='10' height='40' fill='#4fd1c5'>
+                      <animate
+                        attributeName='height'
+                        values='50;70;30;50'
+                        keyTimes='0;0.33;0.66;1'
+                        dur='1s'
+                        repeatCount='indefinite'
+                        calcMode='spline'
+                        keySplines='0.5 0 0.5 1;0.5 0 0.5 1;0.5 0 0.5 1'
+                        begin='-0.2s'
+                      ></animate>
+                    </rect>
+                    <rect x='55' y='15' width='10' height='40' fill='#4fd1c5'>
+                      <animate
+                        attributeName='height'
+                        values='50;70;30;50'
+                        keyTimes='0;0.33;0.66;1'
+                        dur='1s'
+                        repeatCount='indefinite'
+                        calcMode='spline'
+                        keySplines='0.5 0 0.5 1;0.5 0 0.5 1;0.5 0 0.5 1'
+                        begin='-0.6s'
+                      ></animate>
+                    </rect>
+                    <rect x='75' y='15' width='10' height='40' fill='#4fd1c5'>
+                      <animate
+                        attributeName='height'
+                        values='50;70;30;50'
+                        keyTimes='0;0.33;0.66;1'
+                        dur='1s'
+                        repeatCount='indefinite'
+                        calcMode='spline'
+                        keySplines='0.5 0 0.5 1;0.5 0 0.5 1;0.5 0 0.5 1'
+                        begin='-1s'
+                      ></animate>
+                    </rect>
+                  </g>
+                </svg>
+              )}
+              {!!itunes.duration && (
+                <Badge borderRadius='full' px={2} colorScheme='teal'>
+                  {formatDuration(itunes.duration)}
+                </Badge>
+              )}
+              <Text fontSize='14px'>{new Date(isoDate).toLocaleDateString('pt-BR')}</Text>
             </Flex>
-          </Flex>
-          <Flex
-            align='center'
-            gap={2}
-            onClick={playPodcast}
-            cursor='pointer'
-            w='100%'
-            pb={2}
-            pl={{ base: 0, md: 4 }}
-          >
-            {currentPodcast?.enclosure?.url === enclosure?.url && (
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='20px'
-                height='20px'
-                viewBox='0 0 100 100'
-                preserveAspectRatio='xMidYMid'
-              >
-                <g transform='rotate(180 50 50)'>
-                  <rect x='15' y='15' width='10' height='40' fill='#4fd1c5'>
-                    <animate
-                      attributeName='height'
-                      values='50;70;30;50'
-                      keyTimes='0;0.33;0.66;1'
-                      dur='1s'
-                      repeatCount='indefinite'
-                      calcMode='spline'
-                      keySplines='0.5 0 0.5 1;0.5 0 0.5 1;0.5 0 0.5 1'
-                      begin='-0.4s'
-                    ></animate>
-                  </rect>
-                  <rect x='35' y='15' width='10' height='40' fill='#4fd1c5'>
-                    <animate
-                      attributeName='height'
-                      values='50;70;30;50'
-                      keyTimes='0;0.33;0.66;1'
-                      dur='1s'
-                      repeatCount='indefinite'
-                      calcMode='spline'
-                      keySplines='0.5 0 0.5 1;0.5 0 0.5 1;0.5 0 0.5 1'
-                      begin='-0.2s'
-                    ></animate>
-                  </rect>
-                  <rect x='55' y='15' width='10' height='40' fill='#4fd1c5'>
-                    <animate
-                      attributeName='height'
-                      values='50;70;30;50'
-                      keyTimes='0;0.33;0.66;1'
-                      dur='1s'
-                      repeatCount='indefinite'
-                      calcMode='spline'
-                      keySplines='0.5 0 0.5 1;0.5 0 0.5 1;0.5 0 0.5 1'
-                      begin='-0.6s'
-                    ></animate>
-                  </rect>
-                  <rect x='75' y='15' width='10' height='40' fill='#4fd1c5'>
-                    <animate
-                      attributeName='height'
-                      values='50;70;30;50'
-                      keyTimes='0;0.33;0.66;1'
-                      dur='1s'
-                      repeatCount='indefinite'
-                      calcMode='spline'
-                      keySplines='0.5 0 0.5 1;0.5 0 0.5 1;0.5 0 0.5 1'
-                      begin='-1s'
-                    ></animate>
-                  </rect>
-                </g>
-              </svg>
-            )}
-            {!!itunes.duration && (
-              <Badge borderRadius='full' px={2} colorScheme='teal'>
-                {formatDuration(itunes.duration)}
-              </Badge>
-            )}
-            <Text fontSize='14px'>{new Date(isoDate).toLocaleDateString('pt-BR')}</Text>
-          </Flex>
-          <Flex
-            direction='column'
-            flex={1}
-            onClick={playPodcast}
-            cursor='pointer'
-            pl={{ base: 0, md: 4 }}
-          >
-            <Flex overflow='hidden' w='100%' h='115px'>
-              <Text
-                fontSize='14px'
-                color='gray.500'
-                dangerouslySetInnerHTML={{
-                  __html: itunes.summary?.length ? itunes.summary : content,
-                }}
-                sx={{
-                  br: {
-                    display: 'none',
-                  },
-                }}
+            <Flex direction='column' flex={1}>
+              <Flex overflow='hidden' w='100%' h='119px'>
+                <Text
+                  fontSize='14px'
+                  color='gray.500'
+                  dangerouslySetInnerHTML={{
+                    __html: itunes.summary?.length ? itunes.summary : content,
+                  }}
+                  sx={{
+                    br: {
+                      display: 'none',
+                    },
+                  }}
+                />
+              </Flex>
+              <Box
+                bgGradient={`linear(to-b, transparent, ${
+                  colorMode === 'light' ? '#fff' : 'gray.800'
+                })`}
+                h='56px'
+                w='100%'
+                mt='-56px'
               />
             </Flex>
-            <Box
-              bgGradient={`linear(to-b, transparent, ${
-                colorMode === 'light' ? '#fff' : 'gray.800'
-              })`}
-              h='56px'
-              w='100%'
-              mt='-56px'
-            />
           </Flex>
         </Flex>
       </Flex>
